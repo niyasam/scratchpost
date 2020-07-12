16fa5bcdeef0c588e0cab33b3cc0986fdaeeda4b
nmohammedaslam <60123946+nmohammedaslam@users.noreply.github.com>
2020-07-12 00:57:55 -0400

    try4

--- models/user.js
+++ models/user.js
@@ -2,10 +2,16 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String,
+  email: String,
+  phone: String,
+  fullName: String,
+  image: String,
+  imageId: String,
+  joined: { type: Date, default: Date.now }
 });
 
-UserSchema.plugin(passportLocalMongoose)
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
